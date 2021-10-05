190a7d2881df003e81f08f785f48f9ecf7af8c68
Melvin Beemer <melvin@hey.com>
2021-10-06 00:59:17 +0700

    Abstract models out

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/text v0.3.5 // indirect
-	gopkg.in/tucnak/telebot.v2 v2.3.5 // indirect
+	golang.org/x/text v0.3.5
+	gopkg.in/tucnak/telebot.v2 v2.3.5
 )
--- main.go
+++ main.go
@@ -10,48 +10,11 @@ import (
 	"time"
 
 	"github.com/zilstream/bot/helpers"
+	"github.com/zilstream/bot/models"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
-
-type MarketData struct {
-	ATH                   float64 `json:"ath"`
-	ATL                   float64 `json:"atl"`
-	Change24H             float64 `json:"change_24h"`
-	ChangePercentage24H   float64 `json:"change_percentage_24h"`
-	ChangePercentage7D    float64 `json:"change_percentage_7d"`
-	ChangePercentage14D   float64 `json:"change_percentage_14d"`
-	ChangePercentage30D   float64 `json:"change_percentage_30d"`
-	InitSupply            float64 `json:"init_supply"`
-	MaxSupply             float64 `json:"max_supply"`
-	TotalSupply           float64 `json:"total_supply"`
-	CurrentSupply         float64 `json:"current_supply"`
-	DailyVolume           float64 `json:"daily_volume"`
-	MarketCap             float64 `json:"market_cap"`
-	FullyDilutedValuation float64 `json:"fully_diluted_valuation"`
-	CurrentLiquidity      float64 `json:"current_liquidity"`
-	ZilReserve            float64 `json:"zil_reserve"`
-	TokenReserve          float64 `json:"token_reserve"`
-}
-
-type TokenDetail struct {
-	Name                string     `json:"name"`
-	Symbol              string     `json:"symbol"`
-	AddressBech32       string     `json:"address_bech32"`
-	Icon                string     `json:"icon"`
-	Decimals            float64    `json:"decimals"`
-	Website             string     `json:"website"`
-	Whitepaper          string     `json:"whitepaper"`
-	Listed              bool       `json:"listed"`
-	CurrentSupply       float64    `json:"current_supply"`
-	DailyVolume         float64    `json:"daily_volume"`
-	SupplySkipAddresses string     `json:"supply_skip_addresses"`
-	MarketCap           float64    `json:"market_cap"`
-	Rate                float64    `json:"rate"`
-	RateUSD             float64    `json:"rate_usd"`
-	MarketData          MarketData `json:"market_data"`
-}
 
 func main() {
 	b, err := tb.NewBot(tb.Settings{
@@ -90,7 +53,7 @@ func main() {
 			return
 		}
 
-		var token TokenDetail
+		var token models.TokenDetail
 		err = json.Unmarshal(body, &token)
 		if err != nil {
 			b.Send(m.Sender, fmt.Sprintf("Couldn't find %s, type /help for more information.", symbol))
@@ -154,7 +117,7 @@ func main() {
 			return
 		}
 
-		var token TokenDetail
+		var token models.TokenDetail
 		err = json.Unmarshal(body, &token)
 		if err != nil {
 			b.Send(m.Sender, fmt.Sprintf("Couldn't find %s, type /help for more information.", symbol))
